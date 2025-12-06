95a61c25997a51af21f646fa51b004bbd6d9d749
LavanyaAU <lavanyaujjinappa22@gmail.com>
2025-12-06 22:32:08 +0530

    Design List Department Component

--- ems-frontend/src/components/ListDepartmentComponent.jsx
+++ ems-frontend/src/components/ListDepartmentComponent.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ListDepartmentComponent = () => {
+  let dummyData = [
+    {
+      "id":1,
+      "departmentName":"R&D",
+      "departmentDescription":"Research and Development Department"
+    },
+    {
+      "id":2,
+      "departmentName":"Finance",
+      "departmentDescription":"Finance Department"
+    },
+    {
+      "id":3,
+      "departmentName":"Sports",
+      "departmentDescription":"Sports Department"
+    },
+  ]
+
+  const [departments, setDepartments] = useState(dummyData); 
+
   return (
-    <div>ListDepartmentComponent</div>
+    <div className='container'>
+      <h2 className='text-center'>List of Departments</h2>
+      <table className='table table-striped table-bordered'>
+        <thead>
+          <tr>
+            <th>Department Id</th>
+            <th>Department Name</th>
+            <th>Department Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            departments.map( department =>
+                <tr key={department.id}>
+                    <td>{department.id}</td>
+                    <td>{department.departmentName}</td>
+                    <td>{department.departmentDescription}</td>
+                </tr>
+
+              )
+          }
+        </tbody>
+
+      </table>
+    </div>
   )
 }
 
